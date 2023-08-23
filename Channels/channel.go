d8f4c170b47113514820353c0e76cd24e238c42e
abhishek-rabidas <avisec07@gmail.com>
2023-08-23 19:27:55 +0530

    close channel

--- Channels/channel.go
+++ Channels/channel.go
@@ -8,16 +8,25 @@ import (
 func main() {
 	messageChannel := make(chan string)
 
+	//async function to store messages into channel
 	go func() {
-		for {
+		for i := 0; i < 5; i++ {
 			messageChannel <- "time: " + time.Now().String()
 		}
+		close(messageChannel) //closing the channel
 	}()
 
+	//getting messages from channel and printing
 	for {
 		msg := <-messageChannel
 
+		if msg == "" {
+			break
+		}
+
 		fmt.Println(msg)
 	}
+
+	fmt.Println("END")
 
 }
