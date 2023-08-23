f7679bfb8f09a59e9295b74897b89f6758265242
abhishek-rabidas <avisec07@gmail.com>
2023-08-23 20:34:05 +0530

    go routine async api response handle

--- Routines/go_routine.go
+++ Routines/go_routine.go
@@ -3,18 +3,28 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 func main() {
 
-	r, err := http.Get("https://jsonplaceholder.typicode.com/todos")
+	for {
+		rand.Seed(time.Now().UnixNano())
+		min := 0
+		max := 199
+		randomID := rand.Intn(max-min+1) + min
 
-	if err != nil {
-		fmt.Println("Error in fetching data")
+		r, err := http.Get("https://jsonplaceholder.typicode.com/todos/" + strconv.Itoa(randomID))
+
+		if err != nil {
+			fmt.Println("Error in fetching data")
+		}
+
+		go handleResponse(r)
 	}
-
-	handleResponse(r)
 
 }
 
@@ -22,6 +32,7 @@ func handleResponse(response *http.Response) {
 	defer response.Body.Close()
 
 	responseData, err := ioutil.ReadAll(response.Body)
+
 	if err != nil {
 		fmt.Println("Error reading response body:", err)
 		return
