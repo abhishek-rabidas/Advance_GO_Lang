ecd35f9114c6df378a3d6579940b48d1c1e798a4
abhishek-rabidas <avisec07@gmail.com>
2023-08-27 20:51:35 +0530

    base 64 std decoder

--- Decoding/base64.go
+++ Decoding/base64.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"encoding/base64"
+	"fmt"
+)
 
+func main() {
+	encodedHash := "YWJoaXNoZWs="
+
+	decodedStr, _ := base64.StdEncoding.DecodeString(encodedHash)
+	fmt.Println("Decoded string: ", string(decodedStr))
 }
