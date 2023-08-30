a3442a7a25f8cf8b99bf8f0bad49d0a8b9bdc45e
abhishek-rabidas <avisec07@gmail.com>
2023-08-30 11:45:07 +0530

    benchmarking with reverse string method

--- Benchmarking/benchmarking_test.go
+++ Benchmarking/benchmarking_test.go
@@ -1,1 +1,26 @@
-package Benchmarking
+package main
+
+import (
+	"fmt"
+	"github.com/labstack/gommon/random"
+	"testing"
+)
+
+func reverseString() {
+	str := random.Alphanumeric
+	runes := []rune(str)
+	length := len(runes)
+	for i := 0; i < length/2; i++ {
+		runes[i], runes[length-i-1] = runes[length-i-1], runes[i]
+	}
+
+	fmt.Println("Actual string: ", str)
+	fmt.Println("Reversed string: ", string(runes))
+}
+
+func BenchmarkRevString(t *testing.B) {
+
+	for i := 0; i < t.N; i++ {
+		reverseString()
+	}
+}
