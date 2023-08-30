c0a4a8e71b806eac977102f4b23637e501323ee0
abhishek-rabidas <avisec07@gmail.com>
2023-08-30 11:31:29 +0530

    table driven method

--- UnitTesting/sample_test.go
+++ UnitTesting/sample_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // for a test file we need to postfix name the file _test, test function in Go includes this signature: func TestXxxx(t *testing.T) and then run the command go test in the package
 
@@ -21,6 +24,36 @@ func TestBasic(t *testing.T) {
 }
 
 // table driven method
+
+type TestCases struct {
+	a, b int
+	ans  int
+}
+
 func TestTableDriven(t *testing.T) {
 
+	testCases := []TestCases{{
+		a:   5,
+		b:   10,
+		ans: 10,
+	},
+		{
+			a:   7,
+			b:   4,
+			ans: 7,
+		},
+		{
+			a:   9,
+			b:   20,
+			ans: 20,
+		}}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("max(%d, %d)", tc.a, tc.b), func(t *testing.T) {
+			ans := getMaximum(tc.a, tc.b)
+			if ans != tc.ans {
+				t.Error("Wrong")
+			}
+		})
+	}
 }
