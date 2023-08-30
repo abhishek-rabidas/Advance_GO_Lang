186871980635701ba77fb6cd23c84e2b7501ef83
abhishek-rabidas <avisec07@gmail.com>
2023-08-30 11:04:44 +0530

    naming corrected & basic test case add

--- UnitTesting/sample_test.go
+++ UnitTesting/sample_test.go
@@ -2,6 +2,8 @@ package main
 
 import "testing"
 
+// for a test file we need to postfix name the file _test, test function in Go includes this signature: func TestXxxx(t *testing.T) and then run the command go test in the package
+
 func getMaximum(a, b int) int {
 	if a > b {
 		return a
@@ -10,9 +12,15 @@ func getMaximum(a, b int) int {
 	}
 }
 
-func basicTest(t *testing.T) {
+// basic test method
+func TestBasic(t *testing.T) {
 	ans := getMaximum(2, 5)
 	if ans != 5 {
 		t.Error("Wrong")
 	}
+}
+
+// table driven method
+func TestTableDriven(t *testing.T) {
+
 }
