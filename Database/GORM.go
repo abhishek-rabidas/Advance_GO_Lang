8c549d3822843a5a4882a5fb10d3237ad9de935e
abhishek-rabidas <avisec07@gmail.com>
2023-09-11 13:06:44 +0530

    crud operations

--- Database/GORM.go
+++ Database/GORM.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -16,7 +17,49 @@ func main() {
 	var db *gorm.DB = InitializeDB()
 
 	// C -> Create database table record
+	profile1 := Profile{
+		FirstName: "Abhishek",
+		LastName:  "Kumar",
+		Address:   "C-2224, Sector 43",
+		Contact:   "9661572653",
+		IsActive:  true,
+		Company:   "Synergy Labs",
+	}
 
+	profile2 := Profile{
+		FirstName: "Sid",
+		LastName:  "Kumar",
+		Address:   "Sikanderpur",
+		Contact:   "8210xxxxx",
+		IsActive:  true,
+		Company:   "KRITIKAL",
+	}
+
+	db.Create(&profile1)
+	db.Create(&profile2)
+
+	// R -> Read database records
+	var profiles []Profile
+
+	db.Find(&profiles)
+	fmt.Printf("%+v", profiles)
+
+	var profileWithId Profile
+
+	db.Find(&profileWithId, Profile{Model: gorm.Model{ID: 2}})
+	fmt.Printf("%+v", profileWithId)
+
+	// U -> Update database record
+	var newProfile Profile
+	db.First(&newProfile, Profile{Model: gorm.Model{ID: 2}})
+
+	newProfile.FirstName = "Siddheswar"
+	newProfile.LastName = "Ojha"
+
+	db.Save(&newProfile)
+
+	// D -> Delete database record
+	db.Delete(&Profile{}, Profile{Model: gorm.Model{ID: 3}})
 }
 
 func InitializeDB() *gorm.DB {
